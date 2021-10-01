0d4d7c5c73563a952f0c209f8df66fa163596df5
Eileen Chang <407402289@gapp.fju.edu.tw>
2021-10-01 14:15:02 +0800

    <delete> unused code

--- src/Route.js
+++ src/Route.js
@@ -2,9 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import login from './Component/login'
 import Home from './Component/home'
-import Customer from './Component/customer/customer'
-import Changepw from './Component/changepw'
-import Changemenu from './Component/changemenu'
 import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.css'
 // import 'bootstrap/dist/js/bootstrap.js';
@@ -14,9 +11,6 @@ ReactDOM.render(
         <Switch>
             <Route exact path="/" component={login} />
             {localStorage.getItem('name') ? <Route path="/home" component={Home} /> : <Redirect to="/" />}
-            <Route path="/customer" component={Customer} />
-            <Route path="/changepw" component={Changepw} />
-            <Route path="/changemenu" component={Changemenu} />
         </Switch>
     </BrowserRouter>,
     document.getElementById('root'),
