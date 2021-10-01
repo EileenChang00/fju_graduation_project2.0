26d88fea8a35ff04a5e1faa5ceed6aab1772179f
Eileen Chang <407402289@gapp.fju.edu.tw>
2021-10-01 12:27:30 +0800

    <delete> unused file

--- src/index.js
+++ src/index.js
@@ -1,19 +1,22 @@
-//import React from "react";
-
+import React from 'react'
 import ReactDOM from 'react-dom'
 import login from './Component/login'
 import Home from './Component/home'
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import Customer from './Component/customer/customer'
+import Changepw from './Component/changepw'
+import Changemenu from './Component/changemenu'
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.css'
 // import 'bootstrap/dist/js/bootstrap.js';
-import PrivateRoute from './Component/PrivateRoute'
 
 ReactDOM.render(
     <BrowserRouter>
         <Switch>
             <Route exact path="/" component={login} />
-
-            <PrivateRoute path="/home" component={Home} />
+            {localStorage.getItem('name') ? <Route path="/home" component={Home} /> : <Redirect to="/" />}
+            <Route path="/customer" component={Customer} />
+            <Route path="/changepw" component={Changepw} />
+            <Route path="/changemenu" component={Changemenu} />
         </Switch>
     </BrowserRouter>,
     document.getElementById('root'),
